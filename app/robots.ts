82a8bf9eefd2c03f8a7a76ec1693e2043742eace
Steeda Performa <steedamkt@gmail.com>
2026-02-10 15:58:44 -0300

    fix: revert to server mode for Hostinger (remove output: export)

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,6 @@
 import { MetadataRoute } from 'next';
 import { SITE_URL } from '@/lib/constants';
 
-export const dynamic = "force-static";
-
 export default function robots(): MetadataRoute.Robots {
     return {
         rules: {
